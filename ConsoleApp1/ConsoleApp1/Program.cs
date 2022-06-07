c365187cbdb8a4b9d075eaea2c41e2323a1b3558
jose <jose.b4.1992.desarrollo@gmail.com>
2022-06-07 19:38:47 +0200

    examen3evaluacion Jose bernal García

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,80 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
-{
-    // Clase bill
-    // Dada una cantidad, la desglosa en billetes de 50, 20, 10
-    //
-    public class bill
+﻿namespace ConsoleApp1
     {
-        // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
+    using System;
 
-        // constructor vacio, inicializa los atributos
+    /// <summary>
+    /// Dada una cantidad, la desglosa en billetes de 50, 20, 10.
+    /// </summary>
+    public class bill
+        {
+        /// <summary>
+        /// Declaramos los campos: número de billetes.
+        /// </summary>
+        private int billeteCincuenta;
+        private int billeteVeinte;
+        private int billeteDiez;
+
+       /// <summary>
+       /// Setters y getters
+       /// </summary>
+        public int BilleteDiez { get => billeteDiez; set => billeteDiez = value; }
+        public int BilleteCincuenta { get => billeteCincuenta; set => billeteCincuenta = value; }
+        public int BilleteVeinte { get => billeteVeinte; set => billeteVeinte = value; }
+
+        /// <sumary> constructor vacio, inicializa los atributos</sumary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="bill"/> class.
+        /// </summary>
         public bill()
-        {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
-        }
+            {
+            billeteCincuenta = 0;
+            BilleteVeinte = 0;
+            BilleteDiez = 0;
+            }
 
-        // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
-        {
-            if ((c % 10) != 0)
+
+        /// <summary>
+        /// este método permite cambiar la cantidad total y recalcular.
+        /// </summary>
+        /// <param name="cantidad"> cantidad que vamos a ingresar .</param>
+        public void EstablecerCantidad(int cantidad)
+            {
+          
+            const int dividirPor10 = 10;
+            const int dividirPor20 = 20;
+            const int dividirPor50 = 50;
+
+
+            if ((cantidad % dividirPor10) != 0)
                 throw new ArgumentOutOfRangeException();
 
-            if ((c >= 50))
-            {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
+            if ((cantidad >= dividirPor50))
+                {
+                billeteCincuenta = (cantidad / 50);
+                cantidad = cantidad - (billeteCincuenta * 50);
+                }
+            if ((cantidad >= dividirPor20))
+                {
+                BilleteVeinte = (cantidad / 20);
+                cantidad = cantidad - (BilleteVeinte * 20);
+                }
+            if ((cantidad >= 10))
+                {
+                BilleteDiez = (cantidad / 10);
+                cantidad = cantidad - (BilleteDiez * 10);
+                }
             }
-            if ((c >= 20))
-            {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
-            }
-            if ((c >= 10))
-            {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
-            }
-
         }
-    }
 
-    class Program
-    {
-        // El siguiente programa calcula la cantidad necesaria de billetes de
-        // cada tipo para una cantidad dada.
-        // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
-        static void Main(string[] args)
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    internal class Program
         {
-            int cant;
-            bill b = new bill();
+        /// <summary>
+        /// <para>El siguiente programa calcula la cantidad necesaria de billetes de cada tipo para una cantidad dada.</para>
+        ///<para>Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción</para>.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        internal static void Main(string[] args)
+            {
+            int cantidad;
+            bill billetes = new bill();
 
             string linea;
             Console.Write("Introduzca una cantidad: ");
             linea = Console.ReadLine();
-            cant = int.Parse(linea);
+            cantidad = int.Parse(linea);
 
             try
-            {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
-            }
+                {
+                billetes.EstablecerCantidad(cantidad);
+                Console.WriteLine("BILLETES DE 50 : " + billetes.BilleteCincuenta);
+                Console.WriteLine("BILLETES DE 20 : " + billetes.BilleteVeinte);
+                Console.WriteLine("BILLETES DE 10 : " + billetes.BilleteDiez);
+                }
             catch
-            {
+                {
                 Console.WriteLine("Cantidad no válida.");
-            }
+                }
 
             Console.Write("Pulse una Tecla."); Console.ReadLine();
+            }
         }
     }
-}
--- ConsoleApp1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -10,10 +10,43 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            int cantidad = 280;
+            int cantidad = 230;
             int billetes50 = 4;
             int billetes20 = 1;
             int billetes10 = 1;
-        }
+
+            bill billetes = new bill();
+            billetes.EstablecerCantidad(cantidad);
+            int billetesReales10 = billetes.BilleteDiez;
+            int billetesReales20 = billetes.BilleteVeinte;
+            int billetesReales50 = billetes.BilleteCincuenta;
+
+            Assert.AreEqual(billetes10, billetesReales10, "billetes de 10 incorrectos");
+            Assert.AreEqual(billetes20, billetesReales20, "billetes de 20 incorrectos");
+            Assert.AreEqual(billetes50, billetesReales50, "billetes de 50 incorrectos");
+
+         
+            
+            }
+
+
+
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentOutOfRangeException))]
+        public void EstablecerCantidadOutOfRange()
+            {
+            int cantidad = 325;
+
+            bill billetes = new bill();
+            billetes.EstablecerCantidad(cantidad);
+
+
+            }
+
+
+
+
     }
-}
+
+
+
